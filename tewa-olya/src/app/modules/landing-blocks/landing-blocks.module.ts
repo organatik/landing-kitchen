c263f3361bb76b8ee396b8dceb08f9b649e3ac5f
Admin <1q.expert@gmail.com>
2023-07-01 00:29:30 +0300

    added profile-block empty

--- tewa-olya/src/app/modules/landing-blocks/landing-blocks.module.ts
+++ tewa-olya/src/app/modules/landing-blocks/landing-blocks.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainTopBlockComponent } from './main-top-block/main-top-block.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileBlockComponent } from './profile-block/profile-block.component';
 
 
 
 @NgModule({
     declarations: [
-        MainTopBlockComponent
+        MainTopBlockComponent,
+        ProfileComponent,
+        ProfileBlockComponent
     ],
     exports: [
         MainTopBlockComponent
