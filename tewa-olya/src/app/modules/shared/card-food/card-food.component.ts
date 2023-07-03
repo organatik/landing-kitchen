dff984c54eb1490fa838fa51edb845b1ab492b98
Admin <1q.expert@gmail.com>
2023-07-03 18:15:18 +0300

    firt-iteration

---
+++ tewa-olya/src/app/modules/shared/card-food/card-food.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {CardFoodInterface} from "./interfaces/card-food-interface";
+
+@Component({
+  selector: 'app-card-food',
+  templateUrl: './card-food.component.html',
+  styleUrls: ['./card-food.component.scss']
+})
+export class CardFoodComponent {
+
+  @Input() cardData: CardFoodInterface | undefined;
+
+}
---
+++ tewa-olya/src/app/modules/shared/card-food/interfaces/comment-interface.ts
@@ -0,0 +1,7 @@
+export interface CommentInterface{
+  img: string;
+  title: string;
+  description: string;
+
+
+}
