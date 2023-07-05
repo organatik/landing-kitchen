7beb6ad292d6cac04b4603d4fbbfeb4715b06236
Admin <1q.expert@gmail.com>
2023-07-05 15:26:26 +0300

    mobile view done

--- tewa-olya/src/app/modules/shared/header/header.component.ts
+++ tewa-olya/src/app/modules/shared/header/header.component.ts
@@ -1,17 +1,15 @@
 import {Component} from '@angular/core';
 
-
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
- public menuVariable:boolean = false;
- public menuBtnVariable:boolean = false
-  public menuHeaderVariable:boolean = false
-  openMenu(){
+ public menuVariable = false;
+ public menuBtnVariable = false
+  public menuHeaderVariable = false
+  openMenu = () => {
     this.menuVariable = !this.menuVariable;
     this.menuBtnVariable = !this.menuBtnVariable;
     this.menuHeaderVariable =! this.menuHeaderVariable
