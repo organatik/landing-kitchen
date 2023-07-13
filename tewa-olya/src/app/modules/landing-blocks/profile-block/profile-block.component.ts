1f34573bd4075235a3777081e353e70188b8c3ba
Admin <1q.expert@gmail.com>
2023-07-13 18:19:50 +0300

    dont work wowjs

--- tewa-olya/src/app/modules/landing-blocks/profile-block/profile-block.component.ts
+++ tewa-olya/src/app/modules/landing-blocks/profile-block/profile-block.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgwWowService} from 'ngx-wow'
 
 @Component({
   selector: 'app-profile-block',
   templateUrl: './profile-block.component.html',
   styleUrls: ['./profile-block.component.scss']
 })
-export class ProfileBlockComponent {
+export class ProfileBlockComponent implements OnInit {
+
+  constructor(private wowService: NgwWowService) {
+  }
+  ngOnInit(): void {
+    this.wowService.init()
+  }
+  reset(){
+    this.wowService.init()
+  }
+
 
 }
