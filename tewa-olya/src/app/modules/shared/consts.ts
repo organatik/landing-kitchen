c31dd9f1294fdad97d57429ea53cb0cad4a8b3ec
Admin <1q.expert@gmail.com>
2023-07-23 14:38:43 +0300

    added obj with chatId information for users & fix afterpopup style

---
+++ tewa-olya/src/app/modules/shared/consts.ts
@@ -0,0 +1,19 @@
+export const chatId =[
+  {
+    nameId : 'myChatId',
+    numberId: '5075411916'
+  },
+  {
+    nameId : 'chatIdVakulenkoVasyl',
+    numberId: '664645421'
+  },
+  {
+    nameId : 'chatIdSeredaOlga',
+    numberId: '5135402412'
+  },
+  {
+    nameId : 'chatIdVakulenkoYuliia',
+    numberId: '125606045'
+  },
+]
+
--- tewa-olya/src/app/modules/shared/services/telegram-form-bot.ts
+++ tewa-olya/src/app/modules/shared/services/telegram-form-bot.ts
@@ -1,28 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {chatId} from "../consts";
 
 @Injectable({
   providedIn: 'root',
 })
 
-
 export class TelegramFormBotService {
   private botToken = '6508132109:AAGPMx2dS0-plEW_eTYRaievq6BIZ6czqC4';
-  private myChatId = '5075411916'
-  private chatIdVakulenkoVasyl = '664645421';
-  private chatIdSeredaOlga = '5135402412';
-  private chatIdVakulenkoYuliia = '125606045';
-  private chatIds = [this.myChatId, this.chatIdVakulenkoVasyl, this.chatIdSeredaOlga, this.chatIdVakulenkoYuliia];
+
+  // private myChatId = '5075411916'
+  // private chatIdVakulenkoVasyl = '664645421';
+  // private chatIdSeredaOlga = '5135402412';
+  // private chatIdVakulenkoYuliia = '125606045';
+  // private chatIds = [this.myChatId, this.chatIdVakulenkoVasyl, this.chatIdSeredaOlga, this.chatIdVakulenkoYuliia];
+  private chatIds = chatId
+
 
 
   constructor(private httpService: HttpClient) {
   }
     public sendForm(formData: string) {
-      this.chatIds.forEach((chatId) =>{
+      this.chatIds.forEach((chat) =>{
+        console.log(chat.numberId, chat.nameId)
         this.httpService
           .get(
-            `https://api.telegram.org/bot${this.botToken}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${formData}`,
-
+            `https://api.telegram.org/bot${this.botToken}/sendMessage?chat_id=${chat.numberId}&parse_mode=html&text=${formData}`,
           )
           .subscribe();
       })
