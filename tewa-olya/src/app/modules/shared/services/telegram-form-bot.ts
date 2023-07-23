5a3878deed26de8d72b34f814302b51babcc684a
Admin <1q.expert@gmail.com>
2023-07-23 13:56:45 +0300

    fix-for-build-path-img

--- tewa-olya/src/app/modules/shared/services/telegram-form-bot.ts
+++ tewa-olya/src/app/modules/shared/services/telegram-form-bot.ts
@@ -6,37 +6,6 @@ import { HttpClient } from '@angular/common/http';
 })
 
 
-// export class TelegramFormBotService {
-//   private botToken = '6508132109:AAGPMx2dS0-plEW_eTYRaievq6BIZ6czqC4';
-//   private chatId = '338046651';
-//   private chatIdVasyl = '664645421';
-//   private chatIdSeredaOlga = '5135402412';
-//
-//   private chatIdVakulenkoYuliia = '125606045'
-//
-//   constructor(private httpService: HttpClient) {}
-//
-//   public sendForm(formData: string) {
-//     this.httpService
-//       .get(
-//         `https://api.telegram.org/bot${this.botToken}/sendMessage?chat_id=${this.chatIdVakulenkoYuliia}&parse_mode=html&text=${formData}`,
-//
-//       )
-//       .subscribe();
-//   }
-// }
-
-
-//
-// .get(
-//   `https://api.telegram.org/bot${this.botToken}/sendMessage?chat_id=${this.chatIdVasyl}&parse_mode=html&text=${formData}`,
-// )
-//   .get(
-//     `https://api.telegram.org/bot${this.botToken}/sendMessage?chat_id=${this.chatIdSeredaOlga}&parse_mode=html&text=${formData}`,
-//   )
-
-
-
 export class TelegramFormBotService {
   private botToken = '6508132109:AAGPMx2dS0-plEW_eTYRaievq6BIZ6czqC4';
   private myChatId = '5075411916'
@@ -59,3 +28,4 @@ export class TelegramFormBotService {
       })
     }
 }
+
